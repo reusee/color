cae796a807fc70d03a42d249418be1ff4518748e
reus <reusee@gmail.com>
2024-01-27 11:09:59 +0800

    update

--- color.go
+++ color.go
@@ -3,41 +3,27 @@ package color
 import "image/color"
 
 var (
-	//Black        = color.RGBA{0x55, 0x55, 0x55, 0}
-	//Red          = color.RGBA{0xff, 0x82, 0x72, 0}
-	//Green        = color.RGBA{0xb4, 0xfa, 0x72, 0}
-	//Yellow       = color.RGBA{0xfe, 0xfd, 0xc2, 0}
-	//Blue         = color.RGBA{0xad, 0xd5, 0xfe, 0}
-	//Magenta      = color.RGBA{0xff, 0x8f, 0xfd, 0}
-	//Cyan         = color.RGBA{0xd0, 0xd1, 0xfe, 0}
-	//White        = color.RGBA{0xf3, 0xf3, 0xf3, 0}
-	//LightBlack   = color.RGBA{0x66, 0x66, 0x66, 0}
-	//LightRed     = color.RGBA{0xff, 0xc4, 0xbd, 0}
-	//LightGreen   = color.RGBA{0xd6, 0xfc, 0xb9, 0}
-	//LightYellow  = color.RGBA{0xfe, 0xfd, 0xd5, 0}
-	//LightBlue    = color.RGBA{0xc1, 0xe3, 0xfe, 0}
-	//LightMagenta = color.RGBA{0xff, 0xb1, 0xfe, 0}
-	//LightCyan    = color.RGBA{0xe5, 0xe6, 0xfe, 0}
-	//LightWhite   = color.RGBA{0xfe, 0xff, 0xff, 0}
+	Black      = color.RGBA{0x33, 0x33, 0x33, 0}
+	LightBlack = color.RGBA{0x44, 0x44, 0x44, 0}
+	White      = color.RGBA{0xf0, 0xf0, 0xf0, 0}
+	LightWhite = color.RGBA{0xff, 0xff, 0xff, 0}
 
-	Black              = color.RGBA{0x33, 0x33, 0x33, 0}
-	LightBlack         = color.RGBA{0x44, 0x44, 0x44, 0}
-	White              = color.RGBA{0xf0, 0xf0, 0xf0, 0}
-	LightWhite         = color.RGBA{0xff, 0xff, 0xff, 0}
-	n1           uint8 = 0xe6
-	n2           uint8 = 0xf0
-	Red                = color.RGBA{0xff, n1, n1, 0}
-	LightRed           = color.RGBA{0xff, n2, n2, 0}
-	Green              = color.RGBA{n1, 0xff, n1, 0}
-	LightGreen         = color.RGBA{n2, 0xff, n2, 0}
-	Yellow             = color.RGBA{0xff, 0xff, n1, 0}
-	LightYellow        = color.RGBA{0xff, 0xff, n2, 0}
-	Blue               = color.RGBA{n1, n1, 0xff, 0}
-	LightBlue          = color.RGBA{n2, n2, 0xff, 0}
-	Magenta            = color.RGBA{0xff, n1, 0xff, 0}
-	LightMagenta       = color.RGBA{0xff, n2, 0xff, 0}
-	Cyan               = color.RGBA{n1, 0xff, 0xff, 0}
-	LightCyan          = color.RGBA{n2, 0xff, 0xff, 0}
+	n0 uint8 = 0xff
+	n1 uint8 = 0xe3
+	n2 uint8 = 0xf0
+
+	Red          = color.RGBA{n0, n1, n1, 0}
+	LightRed     = color.RGBA{n0, n2, n2, 0}
+	Green        = color.RGBA{n1, n0, n1, 0}
+	LightGreen   = color.RGBA{n2, n0, n2, 0}
+	Yellow       = color.RGBA{n0, n0, n1, 0}
+	LightYellow  = color.RGBA{n0, n0, n2, 0}
+	Blue         = color.RGBA{n1, n1, n0, 0}
+	LightBlue    = color.RGBA{n2, n2, n0, 0}
+	Magenta      = color.RGBA{n0, n1, n0, 0}
+	LightMagenta = color.RGBA{n0, n2, n0, 0}
+	Cyan         = color.RGBA{n1, n0, n0, 0}
+	LightCyan    = color.RGBA{n2, n0, n0, 0}
 
 	Foreground = White
 	Background = Black
